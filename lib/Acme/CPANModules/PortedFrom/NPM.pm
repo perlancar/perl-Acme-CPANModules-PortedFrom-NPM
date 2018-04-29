3e9ba5584d5e32c378f679336005d1e47207b425
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-04-29 07:03:45 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/NPM.pm
@@ -0,0 +1,35 @@
+package Acme::CPANModules::PortedFrom::NPM;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported (or inspired from) NPM libraries",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            module => 'App::chalk',
+            npm_module => 'chalk',
+            tags => ['cli', 'color'],
+        },
+        {
+            module => 'Inky',
+            npm_module => 'inky',
+            tags => ['html', 'template'],
+        },
+        {
+            module => 'Smart::Options',
+            npm_module => 'optimist',
+            tags => ['html', 'template'],
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
